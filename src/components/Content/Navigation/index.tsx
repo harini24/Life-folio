73c1c17702ac2172baf151c76a081491633c7dd3
harini24 <harinisiva1997@gmail.com>
2022-11-28 21:16:49 +0530

    contains changes to add section name

--- src/components/Content/Navigation/index.tsx
+++ src/components/Content/Navigation/index.tsx
@@ -8,39 +8,66 @@ const NavigationWrapper = styled(Box)(({ theme }) => ({
   flexDirection: "column",
   justifyContent: "center",
   padding: "0 20px",
+  "& .nav-section": {
+    display: "flex",
+    flexDirection: "column",
+  },
+  "& .section-name": {
+    width: 84,
+    fontSize: 12,
+    display: "flex",
+    justifyContent: "center",
+    borderRadius: 10,
+  },
+  "& .nav-item": {
+    margin: "10px 0 0 ",
+    height: 70,
+    width: 70,
+    padding: 5,
+    border: "2px solid #562783",
+    borderRadius: "50%",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    "& img": {
+      height: 58,
+      width: 58,
+    },
+  },
+  "& .active .nav-item": {
+    backgroundColor: "#6489a7",
+  },
+  "& .active .section-name": {
+    backgroundColor: "#6489a7",
+  },
   [theme.breakpoints.down("md")]: {
     padding: "0 8px",
     flexDirection: "row",
-  },
-}));
-const NavWrapper = styled(Box)(({ theme }) => ({
-  margin: "10px 0",
-  height: 70,
-  width: 70,
-  padding: 5,
-  border: "2px solid #562783",
-  borderRadius: "50%",
-  display: "flex",
-  justifyContent: "center",
-  alignItems: "center",
-  "& img": {
-    height: 58,
-    width: 58,
   },
 }));
 export const NavigationList = () => {
   const dispatch = useDispatch();
   const sections =
     useSelector((state: IRootState) => state.sections?.sections) || [];
+  const selectedSection =
+    useSelector((state: IRootState) => state.sections?.currentSection) || "";
   console.log(sections);
   return (
     <NavigationWrapper>
       {sections
         .filter((sec) => !sec?.hide)
         .map((sec) => (
-          <NavWrapper onClick={() => dispatch(setCurrentSection(sec.key))}>
-            <img src={sectionImages[sec.key]} alt={sec.key} />
-          </NavWrapper>
+          <Box
+            className={`nav-section ${selectedSection === sec.key && "active"}`}
+          >
+            <Box
+              className="nav-item"
+              onClick={() => dispatch(setCurrentSection(sec.key))}
+            >
+              <img src={sectionImages[sec.key]} alt={sec.key} />
+            </Box>
+            <div className="section-name">{sec.name}</div>
+          </Box>
         ))}
     </NavigationWrapper>
   );
